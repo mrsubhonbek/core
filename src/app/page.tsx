42828073a0bd90b24e6c8fa6ebe04b539ec1262d
subhonbek <basubhonbek@gmail.com>
2024-02-05 15:29:27 +0300

    chor: connection db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/shared/ui/button";
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
 
-export default function Home() {
+export default async function Home() {
+  const course = await db.course.findMany();
+  console.log(course);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>test</Button>
