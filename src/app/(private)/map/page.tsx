f47160b59980ee1319161eafae85e1e73164c557
subhonbek <basubhonbek@gmail.com>
2024-02-09 18:34:56 +0300

    feat/header: add role in header

---
+++ src/app/(private)/map/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Map() {
+  return <div className="container my-10">Map</div>;
+}
+
+export default Map;
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,12 +3,8 @@ import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form
 
 export default async function Home() {
   return (
-<<<<<<< HEAD:src/app/(public)/page.tsx
     <main className="container flex flex-col py-10 items-center">
-=======
-    <main className="container flex flex-col min-h-screen py-10 items-center">
       <h1>Courses</h1>
->>>>>>> master:src/app/page.tsx
       <CreateCourseForm revalidatePagePath="/" className="min-w-80 mb-10" />
       <CoursesList revalidatePagePath="/" />
     </main>
